b29452ae2486c2b98eae4bc019e284b45a5da942
Cyntax <info@cyntax.nl>
2023-03-06 17:25:54 +0100

    Serializer stuff

--- PowerBoss.Infra.Database.MongoDb/Documents/Tesla/VehicleDocument.cs
+++ PowerBoss.Infra.Database.MongoDb/Documents/Tesla/VehicleDocument.cs
@@ -7,14 +7,17 @@ namespace PowerBoss.Infra.Database.MongoDb.Documents.Tesla;
 [Collection("vehicles")]
 public sealed class VehicleDocument
 {
+    [BsonElement(Order = 1)]
     public ObjectId Id { get; set; } = ObjectId.GenerateNewId();
 
     // [BsonRepresentation(BsonType.String)]
+    [BsonElement(Order = 2)]
     [BsonGuidRepresentation(GuidRepresentation.Standard)]
     public Ulid Uuid { get; set; }
 
     public string? Name { get; set; }
     
+    [BsonDefaultValue("currentDate")]
     public DateTimeOffset CreatedOn { get; set; }
     
                                                                                                                                                                                                                                                                                                                                    public DateTimeOffset? UpdatedOn { get; set; }
---
+++ PowerBoss.Infra.Database.MongoDb/Generators/UlidGenerator.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Bson.Serialization;
+
+namespace PowerBoss.Infra.Database.MongoDb.Generators;
+
+public class UlidGenerator : IIdGenerator
+{
+    public static UlidGenerator Instance { get; } = new();
+
+    public object GenerateId(object container, object document)
+    {
+        return Ulid.NewUlid();
+    }
+
+    public bool IsEmpty(object id) 
+        => (Ulid)id == Ulid.Empty;
+}
